a393fc5746ce1abd6972f733f8fe3f4920733477
undstory <undrul.ozog@gmail.com>
2021-04-23 21:02:13 +0200

    styling finished and first data entered

--- src/components/PricingComponent.js
+++ src/components/PricingComponent.js
@@ -1,30 +1,71 @@
 import React from 'react'
 import styles from './PricingComponent.module.scss';
 
-const PricingComponent = () => (
-    <div className={styles.wrapper}>
-        <div className={styles.info}>
-            <span className={styles.views}>100k pageviews</span>
-            <p className={styles.price}>$16.00 <span className={styles.price__month}>/month</span></p>
-        </div>
-        <input className={styles.range} type="range" min="0" max="4" step="1" />
-        <div>
-            <span>Monthly Billing</span>
-            <input type="radio"/>
-            <span>Yearly Billing</span>
-            <span>25% discount</span>
-        </div>
-        <div>Line</div>
-        <div>
-            <ul>
-                <li>Unlimited websites</li>
-                <li>100% data ownership</li>
-                <li>Email reports</li>
-            </ul>
-            <button>Start my trial</button>
-        </div>
-    </div>
-)
+const pricePerViewsValues = [
+    {
+        amount: '10K', 
+        price: '$8'
+    },
+    {
+        amount: '50K', 
+        price: '$12'
+    },
+    {
+        amount: '100K', 
+        price: '$16'
+    },
+    {
+        amount: '500K', 
+        price: '$24'
+    },
+    {
+        amount: '1M', 
+        price: '$36'
+    }
+]
+
+
+class PricingComponent extends React.Component {
+
+    state = {
+        activeValue: '2'
+    }
+
+    changeRange = (e) => {
+        this.setState({
+            activeValue: e.target.value
+        })
+ 
+    }
+
+    render() {
+        return (
+            <div className={styles.wrapper}>
+                <div className={styles.info}>
+                    <span className={styles.views}>100k pageviews</span>
+                    <p className={styles.price}>$16.00 <span className={styles.price__month}>/month</span></p>
+                </div>
+                <input className={styles.range} id="range" value={this.state.activeValue} onChange={this.changeRange} type="range" min="0" max="4" step="1" />
+                <div className={styles.billing}>
+                    <span className={styles.billing__period}>Monthly Billing</span>
+                    <input type="radio"/>
+                    <input type="radio"/>
+                    <span className={styles.billing__period}>Yearly Billing</span>
+                    <span className={styles.billing__discount}>25% discount</span>
+                </div>
+                <div className={styles.line}></div>
+                <div className={styles.info}>
+                    <ul className={styles.info__list}>
+                        <li className={styles.info__item}>Unlimited websites</li>
+                        <li className={styles.info__item}>100% data ownership</li>
+                        <li className={styles.info__item}>Email reports</li>
+                    </ul>
+                    <button className={styles.info__btn}>Start my trial</button>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default PricingComponent
 

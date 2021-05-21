67b84909bd30a7c3c33023aafc00cf4d2278afa0
undstory <undrul.ozog@gmail.com>
2021-05-21 13:16:05 +0200

    added discount changes

--- src/components/PricingComponent.js
+++ src/components/PricingComponent.js
@@ -5,27 +5,25 @@ import Billing from './Billing'
 const pricePerViewsValues = [
     {
         amount: '10K', 
-        price: '$8.00'
+        price: '8.00',
     },
     {
         amount: '50K', 
-        price: '$12.00'
+        price: '12.00',
     },
     {
         amount: '100K', 
-        price: '$16.00'
+        price: '16.00',
     },
     {
         amount: '500K', 
-        price: '$24.00'
+        price: '24.00',
     },
     {
         amount: '1M', 
-        price: '$36.00'
+        price: '36.00',
     }
 ]
-
-
 class PricingComponent extends React.Component {
 
     state = {
@@ -33,7 +31,6 @@ class PricingComponent extends React.Component {
         yearlyBilling: false
     }
 
-  
     changeRange = (e) => {
         this.setState({
             activeValue: e.target.value
@@ -48,29 +45,42 @@ class PricingComponent extends React.Component {
                 `linear-gradient(90deg, 
                 ${sliderProgress} ${breakpoint}, 
                 ${sliderBackground} ${breakpoint})`;
-            console.log(this.state.activeValue)
         })
     }
 
     changeBilling = (data) => {
      
      if(data === true) {
-         console.log("prawda")
+         this.setState({
+             yearlyBilling: true
+         })
      } else {
-         console.log("nieprawda")
+         this.setState({
+            yearlyBilling: false
+        })
      }
-            
-     
+    }
+
+    discount = (value) => {
+        const priceToNum = Number(pricePerViewsValues[value].price);
+        const discountPrice = priceToNum*.75;
+        const numToString = discountPrice.toString();
+        const outputPrice = numToString + '.00'
+        return outputPrice;
     }
 
     render() {
+        const { activeValue, yearlyBilling } = this.state;
+    
         return (
             <div className={styles.wrapper}>
                 <div className={styles.info}>
-                    <span className={styles.views}>{pricePerViewsValues[this.state.activeValue].amount} pageviews</span>
-                    <p className={styles.price}>{pricePerViewsValues[this.state.activeValue].price}<span className={styles.price__month}>/month</span></p>
+                    <span className={styles.views}>{pricePerViewsValues[activeValue].amount} pageviews</span>
+                    <p className={styles.price}>
+                        ${yearlyBilling ?  this.discount(activeValue) : (pricePerViewsValues[activeValue].price)}
+                    <span className={styles.price__month}>/month</span></p>
                 </div>
-                <input className={styles.range} id="range" value={this.state.activeValue} onChange={this.changeRange} type="range" min="0" max="4" step="1" />
+                <input className={styles.range} id="range" value={activeValue} onChange={this.changeRange} type="range" min="0" max="4" step="1" />
                 <Billing changeBilling={this.changeBilling} />
                 <div className={styles.line}></div>
                 <div className={styles.info}>
